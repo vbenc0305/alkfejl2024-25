db3e9878aeaebfded14ab872c6d5b80d03165b95
Vbenc <vargabence0305@gmail.com>
2024-09-17 08:47:26 +0200

    Add project files.

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hello2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Hello World!");
+                string stg = Console.ReadLine(); ;
+                Console.WriteLine($"Your answer: {stg}");
+            }
+        }
+    }
+}
