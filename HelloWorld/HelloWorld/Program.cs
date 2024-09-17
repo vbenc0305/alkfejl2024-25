89b48152d92bc3835acf9485b5bf700ea15fd45d
Vbenc <vargabence0305@gmail.com>
2024-09-17 09:41:17 +0200

    changes

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,15 +1,33 @@
-﻿using System;
+﻿
 
-namespace Hello2
+using HelloWorld2;
+
+namespace NuGet_test
 {
     class Program
     {
         static void Main(string[] args)
         {
+            while (true)
             {
-                Console.WriteLine("Hello World!");
-                string stg = Console.ReadLine(); ;
-                Console.WriteLine($"Your answer: {stg}");
+                Console.WriteLine("Írjon be egy számot: ");
+                string userInput = Console.ReadLine();
+                int number;
+                if (int.TryParse(userInput, out number))
+                {
+                    number=int.Parse(userInput);
+                    var magician = new DummyClass(number);
+
+                    Console.Write("A kapott számból előálló mágikus szám: ");
+                    Console.WriteLine(magician.CalculateMagicNumber());
+                }
+                else
+                {
+                    Console.WriteLine("Rossz input, csak egész számot vár a program.");
+                }
+                Console.WriteLine();
+
+                
             }
         }
     }
---
+++ HelloWorld/HelloWorld2/DummyClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld2
+{
+    public class DummyClass
+    {
+        private int userInput;
+
+        public DummyClass(int userInput)
+        {
+            this.userInput = userInput;
+        }
+
+        public double CalculateMagicNumber()
+        {
+            try
+            {
+                return 1024 / (userInput - 42);
+            }
+            catch (System.DivideByZeroException)
+            {
+                Console.WriteLine("Nem támogatott mágikus szám");
+                return 0;
+            }
+        }
+    }
+}
